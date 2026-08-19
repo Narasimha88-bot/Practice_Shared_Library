7519e7c04198be2cc569ea769aa6986acd46becc
Narasimha <nellorenarasimha88@gmail.com>
2026-08-19 20:41:35 +0000

    Update Maven settings file ID in deploy script

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,7 +2,7 @@ def call() {
 
     stage('Build & Push to Artifactory') {
             steps {
-                configFileProvider([configFile(fileId: 'aa6e5af3-b7b5-4c85-a784-24b381b1708a', variable: 'MAVEN_SETTINGS')]) {
+                configFileProvider([configFile(fileId: '324d7939-2b89-4485-a851-1a9f0ead8bd5', variable: 'MAVEN_SETTINGS')]) {
                     sh '''
                     echo "🔨 Building & pushing to Artifactory..."
                     mvn clean deploy -s $MAVEN_SETTINGS
