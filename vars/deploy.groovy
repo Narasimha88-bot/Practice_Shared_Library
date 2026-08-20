74a494ce60f5589d4eb0599109f683d53b1b41cd
Narasimha Nellore <nellorenarasimha88@gmail.com>
2026-08-20 01:19:54 +0000

    updated the file

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,7 +4,7 @@ def call() {
             steps {
                 configFileProvider([configFile(fileId: '324d7939-2b89-4485-a851-1a9f0ead8bd5', variable: 'MAVEN_SETTINGS')]) {
                     sh '''
-                    echo "🔨 Building & pushing to Artifactory..."
+                    echo "🔨 Building & pushing to Artifactory..." 
                     mvn clean deploy -s $MAVEN_SETTINGS
                     '''
                 }
