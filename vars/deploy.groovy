f944a44b4c0303ca9f2d193a6e97195c3ac0ce88
Narasimha Nellore <nellorenarasimha88@gmail.com>
2026-08-20 01:40:40 +0000

    modified the deployfile

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,13 +1,11 @@
 def call() {
-
     stage('Build & Push to Artifactory') {
-            steps {
-                configFileProvider([configFile(fileId: '324d7939-2b89-4485-a851-1a9f0ead8bd5', variable: 'MAVEN_SETTINGS')]) {
-                    sh '''
-                    echo "🔨 Building & pushing to Artifactory..." 
-                    mvn clean deploy -s $MAVEN_SETTINGS
-                    '''
-                }
-            }
-}
+        // Use pipeline steps directly, no "steps {}"
+        configFileProvider([configFile(fileId: '324d7939-2b89-4485-a851-1a9f0ead8bd5', variable: 'MAVEN_SETTINGS')]) {
+            sh '''
+                echo "🔨 Building & pushing to Artifactory..."
+                mvn clean deploy -s $MAVEN_SETTINGS
+            '''
+        }
+    }
 }
