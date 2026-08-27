76ffe91d32bdaa5542243d245848c02c88e6987d
Narasimha Nellore <Nellorenarasimha88@gmail.com>
2026-08-27 02:55:11 +0000

    updated the downloadfile

--- vars/download.groovy
+++ vars/download.groovy
@@ -1,7 +1,7 @@
 def call() {
 
 stage('Download Latest Snapshot WAR') {
-                configFileProvider([configFile(fileId: '324d7939-2b89-4485-a851-1a9f0ead8bd5', variable: 'MAVEN_SETTINGS')]) {
+                configFileProvider([configFile(fileId: 'f0b88328-4641-4449-a033-39621f26353b', variable: 'MAVEN_SETTINGS')]) {
                     sh '''
                     echo "📦 Downloading latest snapshot WAR..."
                     mvn -s $MAVEN_SETTINGS org.apache.maven.plugins:maven-dependency-plugin:3.7.0:copy \
